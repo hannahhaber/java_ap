4fcb66d03a46fe6f60ad2d5b035cfef9067abbdd
hannahhaber <80170056+hannahhaber@users.noreply.github.com>
2021-03-06 16:57:21 -0500

    Update longest_streak.java
    Completed the code.

--- longest_streak.java
+++ longest_streak.java
@@ -3,10 +3,41 @@ import java.util.Scanner;
 class longest_streak {
   public static void main(String[] args) {
         
-    String str = CCAAAAATTT!;
+    String str = "";
 
     //Letting the Doing the Big Code Begin!!!
-        
 
+    if (str.length() <= 0){
+      System.out.print("String Empty");
+      return;
+    }
+
+    int CurrentStreakLen = 1;
+    String currentStreakChar = str.substring(0,1);
+    int maxStreakLen = CurrentStreakLen;
+    String streakChar = currentStreakChar;
+    String currentChar = "";
+    
+    for (int i = 1; i < str.length(); i++){
+      currentChar = str.substring(i,i+1);
+      if(currentStreakChar.equals(currentChar)){
+        CurrentStreakLen ++;
+      }else {
+
+        //if this streak is the longest, "save off" the max length
+        if (CurrentStreakLen > maxStreakLen){
+          maxStreakLen = CurrentStreakLen;
+          streakChar = currentStreakChar;
+        }
+
+        //now reset the the current streak variables to the new letter and the streak length to one
+        currentStreakChar = currentChar;
+        CurrentStreakLen = 1;
+      }
+     
+
+    }
+    System.out.print(streakChar);
+    System.out.print(" " + maxStreakLen);
   }
 }
