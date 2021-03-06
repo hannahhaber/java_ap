dbe691bb4d296ddb8ccb52ec87b0ac412d8be3b2
fsr-elite <josephhaber@gmail.com>
2021-03-06 16:22:19 -0500

    Create longest_streak.java
    Work on this HW assignment.

---
+++ longest_streak.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class longest_streak {
+  public static void main(String[] args) {
+        
+    String str = CCAAAAATTT!;
+
+    //Letting the Doing the Big Code Begin!!!
+        
+
+  }
+}
